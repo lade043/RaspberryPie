4059267c4d5da63a30dd5c53efdecd059883e11a
Lars Bogner <lars.bogner@outlook.de>
2020-10-23 13:35:31 +0200

    some minor changes

--- RaspberryPie/flask_backend.py
+++ RaspberryPie/flask_backend.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, make_response
+import json
 
 app = Flask(__name__)
 @app.route("/")
 def index():
     pass
+@app.route("/data/<start>/<end>/")
+def functionName(start, end):
+    data = {}
+    db_entries = getEnvironment(int(start), int(end))
+    data["labels"] = db_entries["temperature"][0]
+    data["temp"] = db_entries["temperature"][1]
+    data["humi"] = db_entries["humidity"][1]
+    return json.dumps(data)
